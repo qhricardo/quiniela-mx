b7dd5fb81fd2b0c8a4d9d7c910384d24304f8b20
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-26 18:17:52 -0600

    Add files via upload

---
+++ docs/assets/js/auth.js
@@ -0,0 +1,5 @@
+document.getElementById('form-login').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Login funcional!'); // Prueba básica
+    // Aquí iría tu lógica de autenticación
+});
