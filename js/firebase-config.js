8e14ad0a2118df08163863f37a7c744a4ab45c71
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-27 13:55:36 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 // Configuración de Firebase (reemplaza con tus datos)
 const firebaseConfig = {
-    apiKey: "AIzaSyA1x...",
-    authDomain: "quiniela-mx.firebaseapp.com",
-    projectId: "quiniela-mx",
-    storageBucket: "quiniela-mx.appspot.com",
-    messagingSenderId: "1234567890",
+    apiKey: "AIzaSyAAtOcJznIwyH0ZMXXiR4WSGWMC6UcnVqE",
+    authDomain: "quiniela-mx-9ef52.firebaseapp.com",
+    projectId: "quiniela-mx-9ef52",
+    storageBucket: "quiniela-mx-9ef52.appspot.com",
+    messagingSenderId: "290980633372",
     appId: "1:1234567890:web:abcd1234..."
 };
 
