400ec152612ea1638be07feda7762b046527d9b8
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-27 15:52:44 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     projectId: "quiniela-mx-9ef52",
     storageBucket: "quiniela-mx-9ef52.appspot.com",
     messagingSenderId: "290980633372",
-    appId: "1:290980633372:web:abcd1234..."
+    appId: "1:290980633372:web:c4b32a64e8e7d2ca6978d9"
 };
 
 // Inicializa Firebase
