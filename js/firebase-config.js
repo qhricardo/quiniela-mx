2ced889491a69f08605a933d25b29808f3f506b8
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-27 17:41:49 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,26 +1,14 @@
 // Configuración de Firebase (¡Reemplaza con tus datos!)
 const firebaseConfig = {
-  apiKey: "AIzaSyAAtOcJznIwyH0ZMXXiR4WSGWMC6UcnVqE",                 // Ej: "AIzaSyD123..."
-  authDomain: "quiniela-mx-9ef52.firebaseapp.com", // Ej: "quiniela-mx-12345.firebaseapp.com"
-  projectId: "quiniela-mx-9ef52",               // Ej: "quiniela-mx-12345"
-  storageBucket: "quiniela-mx-9ef52.firebasestorage.app",  // Ej: "quiniela-mx-12345.appspot.com"
-  messagingSenderId: "290980633372",         // Ej: "1234567890"
-  appId: "1:290980633372:web:c4b32a64e8e7d2ca6978d9D"                        // Ej: "1:1234567890:web:abcd1234..."
+  apiKey: "AIzaSyAAtOcJznIwyH0ZMXXiR4WSGWMC6UcnVqE",                 
+  authDomain: "quiniela-mx-9ef52.firebaseapp.com", 
+  projectId: "quiniela-mx-9ef52",              
+  storageBucket: "quiniela-mx-9ef52.firebasestorage.app",  
+  messagingSenderId: "290980633372",         
+  appId: "1:290980633372:web:c4b32a64e8e7d2ca6978d9D"                      
 };
-firebase.auth().onAuthStateChanged((user) => {
-  const currentPage = window.location.pathname.split('/').pop();
-  if (!user && currentPage !== 'login.html') {
-    window.location.href = 'login.html';
-  }
-});
-// Inicialización segura
-if (typeof firebase !== 'undefined') {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-} else {
-  console.error("Error: Firebase SDK no cargado");
-}
 
-// Exporta el módulo de autenticación
-const auth = firebase.auth();
+// Inicialización segura
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
