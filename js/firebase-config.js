a618c8dc691cc75ea43d776cd8da75459341c2fc
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-28 15:38:02 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,13 @@
-// Configura con TUS datos de Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAAtOcJznIwyH0ZMXXiR4WSGWMC6UcnVqE",                 
-  authDomain: "quiniela-mx-9ef52.firebaseapp.com", 
-  projectId: "quiniela-mx-9ef52",              
-  storageBucket: "quiniela-mx-9ef52.firebasestorage.app",  
-  messagingSenderId: "290980633372",         
-  appId: "1:290980633372:web:c4b32a64e8e7d2ca6978d9D"                      
+  apiKey: "AIzaSyAAtOcJznIwyH0ZMXXiR4WSGWMC6UcnVqE",
+  authDomain: "quiniela-mx-9ef52.firebaseapp.com",
+  databaseURL: "https://quiniela-mx-9ef52-default-rtdb.firebaseio.com",
+  projectId: "quiniela-mx-9ef52",
+  storageBucket: "quiniela-mx-9ef52.firebasestorage.app",
+  messagingSenderId: "290980633372",
+  appId: "1:290980633372:web:c4b32a64e8e7d2ca6978d9",
+  measurementId: "G-9SVDHK680L"
 };
 
 // Inicialización segura
