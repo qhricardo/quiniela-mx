4305c6ce40c77e69f33be8cda0c401545996b865
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-28 13:08:40 -0600

    Delete routes directory

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, register } = require('../controllers/authController');
-const { loginValidator, registerValidator } = require('../middlewares/validators');
-
-router.post('/login', loginValidator, login);
-router.post('/register', registerValidator, register);
-
-module.exports = router;
