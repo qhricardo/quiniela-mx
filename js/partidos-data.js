d35babaaa7be289085bee4956f397280cfc8c684
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-30 17:49:15 -0600

    Update partidos-data.js

--- js/partidos-data.js
+++ js/partidos-data.js
@@ -1,12 +1,12 @@
 window.partidosProgramados = [
-  "America vs Chivas",
-  "Equipo C vs Equipo D",
-  "Equipo E vs Equipo F",
-  "Equipo G vs Equipo H",
-  "Equipo I vs Equipo J",
-  "Equipo K vs Equipo L",
-  "Equipo M vs Equipo N",
-  "Equipo O vs Equipo P",
-  "Equipo Q vs Equipo R",
-  "Equipo S vs Equipo T"
+ P1 "America vs Chivas",
+ P2 "Equipo C vs Equipo D",
+ P3 "Equipo E vs Equipo F",
+ P4 "Equipo G vs Equipo H",
+ P5 "Equipo I vs Equipo J",
+ P6 "Equipo K vs Equipo L",
+ P7 "Equipo M vs Equipo N",
+ P8 "Equipo O vs Equipo P",
+ P9 "Equipo Q vs Equipo R",
+ P10 "Equipo S vs Equipo T"
 ];
