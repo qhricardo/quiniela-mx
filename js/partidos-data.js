6a229216682a109e6631c06169a292599a2f31f4
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-30 17:55:38 -0600

    Update partidos-data.js

--- js/partidos-data.js
+++ js/partidos-data.js
@@ -1,12 +1,12 @@
 window.partidosProgramados = [
- P1 "America vs Chivas",
- P2 "Equipo C vs Equipo D",
- P3 "Equipo E vs Equipo F",
- P4 "Equipo G vs Equipo H",
- P5 "Equipo I vs Equipo J",
- P6 "Equipo K vs Equipo L",
- P7 "Equipo M vs Equipo N",
- P8 "Equipo O vs Equipo P",
- P9 "Equipo Q vs Equipo R",
- P10 "Equipo S vs Equipo T"
+  "Partido 1: América vs Pumas",
+  "Partido 2: Chivas vs Tigres",
+  "Partido 3: Monterrey vs Cruz Azul",
+  "Partido 4: Toluca vs Santos",
+  "Partido 5: León vs Atlas",
+  "Partido 6: Querétaro vs Tijuana",
+  "Partido 7: Pachuca vs Necaxa",
+  "Partido 8: Juárez vs San Luis",
+  "Partido 9: Mazatlán vs Puebla",
+  "Partido 10: Morelia vs Celaya"
 ];
