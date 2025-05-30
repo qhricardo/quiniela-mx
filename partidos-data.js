e33db0b8c90ee6e158691389c5787d594f3fc684
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-05-30 17:40:52 -0600

    Update partidos-data.js

--- partidos-data.js
+++ partidos-data.js
@@ -1,13 +1,12 @@
-// partidos-data.js
-const partidosProgramados = [
-  "América vs Pumas",
-  "Tigres vs Monterrey",
-  "Chivas vs Cruz Azul",
-  "León vs Toluca",
-  "Puebla vs Santos",
-  "Atlas vs Necaxa",
-  "Querétaro vs Juárez",
-  "Mazatlán vs Tijuana",
-  "San Luis vs Pachuca",
-  "UNAM vs América"
+window.partidosProgramados = [
+  "Equipo A vs Equipo B",
+  "Equipo C vs Equipo D",
+  "Equipo E vs Equipo F",
+  "Equipo G vs Equipo H",
+  "Equipo I vs Equipo J",
+  "Equipo K vs Equipo L",
+  "Equipo M vs Equipo N",
+  "Equipo O vs Equipo P",
+  "Equipo Q vs Equipo R",
+  "Equipo S vs Equipo T"
 ];
